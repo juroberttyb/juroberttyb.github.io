7d59ea57cd1caccf849bb722fd600dd2b91a15e0
robert <juroberttyb@gmail.com>
2023-01-05 14:37:03 +0800

    refactor: migrate header and footer to react components

---
+++ src/component/Footer.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer id="footer">
+        <ul class="icons">
+            <li><a href="https://twitter.com/zhuboxuan2" class="icon brands alt fa-twitter"><span class="label">Twitter</span></a></li>
+            <li><a href="https://www.facebook.com/juroberttyb/" class="icon brands alt fa-facebook-f"><span class="label">Facebook</span></a></li>
+            <li><a href="https://www.linkedin.com/in/bo-shiuan-chu-5b66081a9/" class="icon brands alt fa-linkedin-in"><span class="label">LinkedIn</span></a></li>
+            <li><a href="https://github.com/juroberttyb" class="icon brands alt fa-github"><span class="label">GitHub</span></a></li>
+            <li><a href="mailto: juroberttyb@gmail.com" class="icon solid alt fa-envelope"><span class="label">Email</span></a></li>
+        </ul>
+        <ul class="copyright">
+            <li>&copy; All rights reserved.</li>
+        </ul>
+    </footer>
+  )
+}
+
+export default Footer
