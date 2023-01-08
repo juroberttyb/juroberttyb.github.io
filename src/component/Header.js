cf06ad7c8ce18889f87ed0b25220191234b9a0dd
robert <juroberttyb@gmail.com>
2023-01-08 20:40:42 +0800

    adjust: remove nav tag

--- src/component/Header.js
+++ src/component/Header.js
@@ -12,15 +12,13 @@ const Header = ({ connected, setConnected }) => {
     return (
         <center>
             <header id="header">
-                <nav>
-                    <ul>
-                        <li><Link to="/">Home</Link></li>
-                        <li><Link to="/about">About</Link></li>
-                        <li><Link to="/publication">Publication</Link></li>
-                        <li><Link to="/quote">Quote</Link></li>
-                        <li><button className="button primary" id="connect wallet" onClick={onClick}>{connected}</button></li>
-                    </ul>
-                </nav>
+                <ul>
+                    <li><Link to="/">Home</Link></li>
+                    <li><Link to="/about">About</Link></li>
+                    <li><Link to="/publication">Publication</Link></li>
+                    <li><Link to="/quote">Quote</Link></li>
+                    <li><button className="button primary" id="connect wallet" onClick={onClick}>{connected}</button></li>
+                </ul>
             </header>
         </center>
     )
