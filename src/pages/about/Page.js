36eec1db0ffa3e7c6ac9b7b2278b3c0bad12af10
robert <juroberttyb@gmail.com>
2023-01-09 22:29:00 +0800

    feature: add url for image

--- src/pages/about/Page.js
+++ src/pages/about/Page.js
@@ -6,7 +6,10 @@ const about = {
         <>
             During my free time, I enjoy taking a walk, playing piano jazz and watching standup comedy.<br/>
             <br/>
-            Also, I made some ecosphere from time to time, although useless, they look nice anyway.
+            Also, I made some ecosphere from time to time, although useless, they look nice anyway.<br/>
+            <br/>
+            <b>Click on image below to see some piano musics I made!</b><br/>
+            <br/>
         </>
     ),
     header: (
@@ -14,7 +17,8 @@ const about = {
             About Me
         </>
     ),
-    img: pianoImg
+    img: pianoImg,
+    imgUrl: "https://www.youtube.com/channel/UCmEH_lLXrnW_L3n6x_4l1Pg",
 }
 
 export default about
