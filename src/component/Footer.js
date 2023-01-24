7c6928ae8dfd155ccebded2f6e15c243768c3b2d
robert <juroberttyb@gmail.com>
2023-01-24 22:17:22 +0800

    update: remove redundant

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
     <footer className="footer center">
         <ul>
             <li><a href="https://twitter.com/zhuboxuan2">Twitter</a></li>
-            <li><a href="https://www.facebook.com/juroberttyb/">Facebook</a></li>
+            {/* <li><a href="https://www.facebook.com/juroberttyb/">Facebook</a></li> */}
             <li><a href="https://www.linkedin.com/in/bo-shiuan-chu-5b66081a9/">LinkedIn</a></li>
             <li><a href="https://github.com/juroberttyb">GitHub</a></li>
             <li><a href="mailto: juroberttyb@gmail.com">Email</a></li>
