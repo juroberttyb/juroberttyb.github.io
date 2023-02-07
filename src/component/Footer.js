69da845e169f0736709026809bb90ee42ef24464
robert <juroberttyb@gmail.com>
2023-02-07 22:37:02 +0800

    fix: linkedin path

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -6,7 +6,7 @@ const Footer = () => {
         <ul>
             <li><a href="https://twitter.com/zhuboxuan2">Twitter</a></li>
             {/* <li><a href="https://www.facebook.com/juroberttyb/">Facebook</a></li> */}
-            <li><a href="https://www.linkedin.com/in/bo-shiuan-chu-5b66081a9/">LinkedIn</a></li>
+            <li><a href="https://www.linkedin.com/in/robert-chu-5b66081a9/">LinkedIn</a></li>
             <li><a href="https://github.com/juroberttyb">GitHub</a></li>
             <li><a href="mailto: juroberttyb@gmail.com">Email</a></li>
         </ul>
