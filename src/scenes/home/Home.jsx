1b181f51cf2d9af20a6dacb268947d070021eaf6
robert <juroberttyb@gmail.com>
2024-06-19 00:55:45 +0800

    update

--- src/scenes/home/Home.jsx
+++ src/scenes/home/Home.jsx
@@ -10,11 +10,11 @@ const Home = () => {
         <div className="page">
             <div id="ohana-description">
                 <div id="ohana-title">Ohana</div>
-                <div id="ohana-slogan">讓您的美金活存利率與世界接軌</div>
+                <div id="ohana-slogan">讓您享受全台最高美元活存利率</div>
                 <div className="ohana-text">
-                    <p>Ohana的開發團隊相信，每個人都該擁有屬於自己的銀行，而我們已成功打造出</p>
+                    <p>Ohana的開發團隊透過與世界各地銀行接軌，成功提供每位用戶</p>
                     <br/>
-                    <p>1 - 與世界接軌的美元活存利率</p>
+                    <p>1 - 全台最高美元活存利率</p>
                     <p>2 - 比台灣銀行更好的美元匯率</p>
                     {/* <p>3 - 永久0手續費的轉帳</p> */}
                 </div>
